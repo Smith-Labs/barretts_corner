e9e932f7e029a75dd0299fc8dea9820501a37681
Gear-Smith <gear_smith@outlook.com>
2023-08-19 21:32:52 -0500

    adds goals route

---
+++ netlify/functions/get-user-goals/get-user-goals.ts
@@ -0,0 +1,29 @@
+import { Handler } from '@netlify/functions'
+import axios from 'axios'
+
+export const handler: Handler = async (event, context) => {
+  const params = event.queryStringParameters
+
+  try {
+
+    const response = await axios.get(`https://api.twitch.tv/helix/goals?broadcaster_id=${params?.user_id}`,
+      {
+        headers: {
+          'Authorization': `Bearer ${params?.token}`,
+          'Client-Id': process.env.CLIENT_ID
+        }
+      })
+
+      console.log(response)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data),
+    }
+  } catch (error) {
+    return {
+        statusCode: 500,
+        body: JSON.stringify({ error: error })
+    };
+  }
+}
